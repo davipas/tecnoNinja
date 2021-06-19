f02faa350f64299c7d0ddf9bc22875e8066c44bd
David Conde <davidca926@gmail.com>
2021-06-19 09:20:53 -0400

    Add files via upload

--- database/db.py
+++ database/db.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
 import psycopg2
 from psycopg2 import pool
+
 from config_reader import NoveConfiguration
 
+
 class Database:
-    def session(self):
-        return self._driver.get_conn()
-    def __repr__(self):
-        return 'DB(url=%s)' % self._host
+
     def __int__(self):
+        pass
+
+    def session(self):
         configuration = NoveConfiguration()
         self._host = configuration.postgres_host
         self._user = configuration.postgres_user
         self._password = configuration.postgres_password
         self._port = configuration.postgres_port
         self._database = configuration.postgres_database
-        self._driver = psycopg2.pool.SimpleConnectionPool(1, 20,
-                        host = self._host,
-                        user = self._user,
-                        password = self._password,
-                        port = self._port,
-                        database = self._database)
+        self._driver = psycopg2.connect(host=self._host, user=self._user, password=self._password, port=self._port,
+                                        database=self._database)
         if self._driver:
             print("connection pool created successfully")
+        return self._driver
