d3ee1592bfcd6e7f683018268e8d86599d151cdf
David Conde <davidca926@gmail.com>
2021-06-19 09:23:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-
+from flask import Flask, request, jsonify, abort
+from models.users import Users
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
     return "Bienvenido a tecnoNinja"
+
+
+@app.route('/tecnologianinja/users.browse/<int:id_user>', methods=['GET'])
+def loggin_user(id_user):
+    user = Users({'id_user':id_user})
+    user_id = user.browse_user(id_user)
+    return jsonify({'user_id': user_id}), 200
 
 
 if __name__ == '__main__':
